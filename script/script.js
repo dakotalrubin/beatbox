d47f0bba125c9d86a8863fc509b9c94e74417ceb
dlrubin <dlrubin@ucsc.edu>
2023-07-11 19:46:28 -0700

    Reorganize JS functions into related sections

--- script/script.js
+++ script/script.js
@@ -1,8 +1,12 @@
 // ----------------------------------------------------------------------------
-// Global variables
+// GLOBAL VARIABLES -----------------------------------------------------------
+// ----------------------------------------------------------------------------
+
 let interval; // Interval for playing the beat
 let bpm = 120; // Default 120 bpm
-    //**** new additions for beat playback and latency maintenance */
+let popup = document.getElementById("popup");
+
+// Global variables for beat playback and latency maintenance
 let beatsInLoop = 8;
 let nextNoteTime = 0.0;
 let beat = 0;
@@ -11,35 +15,16 @@ let audioContx = null;
 let scheduleTimeBuf = 0.001;
 let beatsPlaying = false;
 let scheduleFreq = 25;
-let trackVolume = 1; // To be modified when volume button is added 
-// ---------------------------------------------------------------------------- 
-let popup = document.getElementById("popup");
+let trackVolume = 1; // To be modified when changing volume
+
+// ----------------------------------------------------------------------------
+// HEADER TEMPO BUTTON HANDLING -----------------------------------------------
 // ----------------------------------------------------------------------------
 
 // Creates header_tempo
 const header_tempo = document.querySelector(".header-tempo");
 // for header_tempo, adds event listener for "click"
 header_tempo.addEventListener("click", update_header_tempo);
-
-// Creates header_play
-const header_play = document.querySelector(".header-play");
-// for header_play, adds event listener for "click"
-header_play.addEventListener("click", play_beat);
-
-// Creates header_stop
-const header_stop = document.querySelector(".header-stop");
-// for header_stop, adds event listener for "click"
-header_stop.addEventListener("click", stop_beat);
-
-// Creates node list (icpbs) of instrument-channel-play-buttons
-const icpbs = document.querySelectorAll(".instrument-channel-play-button");
-
-// For each instrument-channel-play-button, adds event listener for "click"
-icpbs.forEach((button) => {
-  button.addEventListener("click", play_icpb_sound);
-  
-});
-
 
 // This function updates the tempo button value (the bpm)
 function update_header_tempo() {
@@ -105,8 +90,108 @@ function update_header_tempo() {
   return;
 }
 
-// Play highlighted beats at scheduled time (now) with current audio, and increment beats 
+// ----------------------------------------------------------------------------
+// HEADER PLAY BUTTON HANDLING ------------------------------------------------
+// ----------------------------------------------------------------------------
+
+// Creates header_play
+const header_play = document.querySelector(".header-play");
+// for header_play, adds event listener for "click"
+header_play.addEventListener("click", play_beat);
+
+// ----------------------------------------------------------------------------
+// HEADER STOP BUTTON HANDLING ------------------------------------------------
+// ----------------------------------------------------------------------------
+
+// Creates header_stop
+const header_stop = document.querySelector(".header-stop");
+// for header_stop, adds event listener for "click"
+header_stop.addEventListener("click", stop_beat);
+
+// ----------------------------------------------------------------------------
+// INSTRUMENT CHANNEL HANDLING ------------------------------------------------
+// ----------------------------------------------------------------------------
+
+// Creates node list (icpbs) of instrument-channel-play-buttons
+const icpbs = document.querySelectorAll(".instrument-channel-play-button");
+
+// For each instrument-channel-play-button, adds event listener for "click"
+icpbs.forEach((button) => {
+  button.addEventListener("click", play_icpb_sound);
+});
+
+// This function plays the audio of the clicked instrument-channel-play-button (ICPB)
+function play_icpb_sound(e) {
+
+  // Initializes icpb variable with the clicked ICPB
+  const icpb = e.target;
+
+  // Initializes audio variable with the clicked ICPB's associated sound
+  const sound = icpb.getAttribute('sound');
+  const audio = document.querySelector(`audio[sound="${sound}"]`);
+
+  // Exits function if the clicked ICPB has no audio (unnecessary as of right now)
+  if (!audio) {
+    return;
+  }
+
+  // Plays audio without waiting for previous sound to finish
+  audio.currentTime = 0;
+  audio.play();
+
+  return;
+}
+
+// This function updates an instrument channel's volume button value
+function update_instrument_channel_volume() {
+  // IN PROGRESS
+  return;
+}
+
+// ----------------------------------------------------------------------------
+// AUDIO PLAYBACK HANDLING -----------------------------------------------------
+// ----------------------------------------------------------------------------
+
+// This function plays the beat at the project's bpm
+function play_beat() {
+
+  // Prevents errors from spamming header-play button (multiple intervals at a time)
+  clearInterval(interval);
+  if (beatsPlaying) return; // May need to remove this once adding more tracks!
+
+  // Calculation to turn bpm into time between beats (tbb) in milliseconds
+  let tbb = (60 / bpm) * 1000;
+
+  beatsPlaying = true;
+  beat = 0; // Restart at beginning of loop. Beat variable will always be 1-8
+            // (except when being initialized)
+
+  // Add Audio Context for scheduling
+  if (audioContx == null) {
+    audioContx = new (window.AudioContext || window.webkitAudioContext)();
+  }
+
+  nextNoteTime = audioContx.currentTime + 0.05;
+
+  // Run scheduler and play beats
+  interval = setInterval(() => runSchedulerAndBeat(), scheduleFreq );
+
+  return;
+}
+
+// This function stops the beat and resets to the beginning
+function stop_beat() {
+
+  beatsPlaying = false;
+  beat = 1;
+  clearInterval(interval);
+
+  return;
+}
+
+// Play highlighted notes at scheduled time (now) with current audio and increment beats 
 function playNextBeat() {
+
   // Advance current note and time by a quarter note (crotchet if you're posh)
   var secondsPerBeat = 60.0 / bpm; // Get length of beat
   nextNoteTime += secondsPerBeat; // Add beat length to last beat time
@@ -133,11 +218,14 @@ function playNextBeat() {
   } else {
     highlightElemBackground(instrument_note_button, '#303030');
   }
+
+  return;
 }
 
 // Schedule beat at time now for immediate playback 
 function scheduleBeat(beatNumber, time) {
-  // push beat onto queue, even if played or not
+
+  // Push beat onto queue, even if played or not
   queueBeats.push({ note: beatNumber, time: time });
   const source = audioContx.createGain();
         
@@ -146,85 +234,42 @@ function scheduleBeat(beatNumber, time) {
   source.gain.exponentialRampToValueAtTime(0.001, time + 0.02);
 
   source.connect(audioContx.destination);
-
+  return;
 }
 
 // This function schedules beats for minimal latency, then calls for beat to be played
 function runSchedulerAndBeat(){
-  // while there are notes that will need to play before the next interval, schedule them and advance pointer
+
+  // While there are notes that will need to play before the next interval,
+  // schedule those notes and advance pointer
   while (nextNoteTime < audioContx.currentTime + scheduleTimeBuf ) {
+
     scheduleBeat(beat, nextNoteTime); // Schedule beat for immediate playback 
-    playNextBeat();                   // Play beat at scheduled time (now)
+    playNextBeat(); // Play beat at scheduled time (now)
   }
+
+  return;
 }
+
+// ----------------------------------------------------------------------------
+// INSTRUMENT CHANNEL POPUP WINDOW HANDLING -----------------------------------
+// ----------------------------------------------------------------------------
+
 // This function opens an instrument channel popup window
-function open_popup() {
+function open_instrument_channel_popup() {
   popup.classList.add("open-popup");
   return;
 }
 
 // This function closes an instrument channel popup window
-function close_popup() {
+function close_instrument_channel_popup() {
   popup.classList.remove("open-popup");
   return;
 }
 
-// This function updates the volume button value
-function update_volume() {
-  return;
-}
-
-// This function plays the audio of the clicked instrument-channel-play-button (ICPB)
-function play_icpb_sound(e) {
-
-  // Initializes icpb variable with the clicked ICPB
-  const icpb = e.target;
-
-  // Initializes audio variable with the clicked ICPB's associated sound
-  const sound = icpb.getAttribute('sound');
-  const audio = document.querySelector(`audio[sound="${sound}"]`);
-
-  // Exits function if the clicked ICPB has no audio (unnecessary as of right now)
-  if (!audio) {
-    return;
-  }
-
-  // Plays audio without waiting for previous sound to finish
-  audio.currentTime = 0;
-  audio.play();
-
-  return;
-}
-
-// This function plays the beat at the project's bpm
-function play_beat() {
-  // Prevents errors from spamming header-play button (multiple intervals at a time)
-  clearInterval(interval);
-  if (beatsPlaying) return; // *** may need to remove this once adding more tracks ******
-
-  // Calculation to turn bpm into time between beats (tbb) in milliseconds
-  let tbb = (60 / bpm) * 1000;
-
-  beatsPlaying = true;
-  beat = 0; // Restart at beginning of loop. Beat variable will always be 1-8 (except when being initialized)
-  // *** Add Audio Context for scheduling *** // 
-  if (audioContx == null){
-    audioContx = new (window.AudioContext || window.webkitAudioContext)();
-  }
-  nextNoteTime = audioContx.currentTime + 0.05;
-  // Run scheduler and play beats
-  interval = setInterval(() => runSchedulerAndBeat(), scheduleFreq );
-
-  return;
-}
-
-// This function stops the beat and resets to the beginning. 
-function stop_beat() {
-  beatsPlaying = false;
-  beat = 1;
-  clearInterval(interval);
-  return;
-}
+// ----------------------------------------------------------------------------
+// NOTE GRID HANDLING ---------------------------------------------------------
+// ----------------------------------------------------------------------------
 
 // This function toggles an instrument note button on (orange) or off (gray)
 function note_toggle(id) {
@@ -248,19 +293,19 @@ function note_toggle(id) {
   return;
 }
 
-// This function will highlight the background of any element
-// obj - the affected object
+// This function will highlight the background of any note
+// obj - the affected note
 // color - the color palette to be applied wrapped in quotes
-// e.g. '#fffff' for the color white
+// e.g. "#FFFFFF" for the color white
 function highlightElemBackground(obj, color) {
 
   // Calculation to turn bpm into time between beats (tbb) in milliseconds
   let tbb = (60 / bpm) * 1000;
 
-  // Create timeout so the element flashes with the beat
   const flash = obj.style.backgroundColor;
   obj.style.backgroundColor = color;
 
+  // Create timeout so the note flashes with the beat
   setTimeout(() => {
     obj.style.backgroundColor = flash;
   }, tbb);

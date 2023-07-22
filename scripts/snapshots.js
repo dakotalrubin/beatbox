f0493d94b905247c84990d3f52b602aa8168a562
dlrubin <dlrubin@ucsc.edu>
2023-07-21 17:56:18 -0700

    Starting snapshot upload

--- scripts/snapshots.js
+++ scripts/snapshots.js
@@ -1,3 +1,17 @@
+// ----------------------------------------------------------------------------
+// EVENT LISTENERS FOR SNAPSHOT BUTTONS ---------------------------------------
+// ----------------------------------------------------------------------------
+
+const save_snapshot = document.querySelector(".header-save-snapshot");
+save_snapshot.addEventListener("click", snapshot_download);
+
+const load_snapshot = document.querySelector(".header-load-snapshot");
+load_snapshot.addEventListener("click", snapshot_upload);
+
+// ----------------------------------------------------------------------------
+// SNAPSHOT DOWNLOAD BUTTON ---------------------------------------------------
+// ----------------------------------------------------------------------------
+
 // Retrieves all current sound files and project settings for download
 async function snapshot_download() {
     var zip = new JSZip();
@@ -23,21 +37,52 @@ async function snapshot_download() {
         var filename = sound.split('\\').pop().split('/').pop();
         var file = await fetch(sound);
 
-        // This file was uploaded by the user, so convert blob to usable format
-        // and rename file
+        // This file was uploaded by the user, so convert blob so rename file
         if (filename.slice(-3) != "wav") {
             filename = document.getElementById(`sound-${index}`).getAttribute("data");
         }
 
         // Add sound blobs to zip file
         var sound_blob = await file.blob();
-        var zip_sound = zip.folder("sounds");
+        var zip_sound = zip.folder("beatbox_files");
         zip_sound.file(index + " - " + filename, sound_blob, {base64: true, binary: true});
         count++;
+
+        // Save files and generate zip folder
         if (count == sounds.length + 1) {
+
+            user_data = generate_user_data();
+            var user_data_blob = new Blob([user_data], {type: "text/plain; charset=utf-8"});
+            zip_sound = zip.folder("beatbox_files");
+            zip_sound.file("user_data.txt", user_data_blob);
+
             zip.generateAsync({type: "blob"}).then(function(content) {
                 saveAs(content, zip_filename);
             });
         }
     });
+}
+
+// This function gathers all settings on the site and compiles them into a string
+function generate_user_data() {
+    user_data = "";
+    user_data += `Tempo: ${document.querySelector(".header-tempo").getAttribute("value")};\n`;
+    // user_data += everything else lol
+    return user_data;
+}
+
+// ----------------------------------------------------------------------------
+// SNAPSHOT UPLOAD BUTTON -----------------------------------------------------
+// ----------------------------------------------------------------------------
+
+// Uploads all downloaded sound files and project settings
+async function snapshot_upload() {
+/*
+    var zip = new JSZip();
+    zip.loadAsync(data).then(function(zip) {
+        zip.file("user_data.txt").async("string").then(function(data) {
+            console.log(data); // Print data from "user_data.txt" file
+        });
+    });
+*/
 }
